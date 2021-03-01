cb44dc3a0075390c39b9e0a9f2d033198e72ea02
Ian <ichen2@lsu.edu>
2021-03-01 14:20:12 -0600

    Created basic synth, added css to make it look like keyboard

--- sketch.js
+++ sketch.js
@@ -1,12 +1,34 @@
 const SCREEN_WIDTH = 1280;
 const SCREEN_HEIGHT = 800;
+const keys = [];
+const scale = ['C4', 'D4', 'E4', 'G4', 'A4', 'C5'];
+
+class Key {
+  constructor(note) {
+    keys.push(this);
+    this.note = note;
+    this.button = createButton(note);
+    this.button.position(100 * keys.length, 100);
+    this.button.mousePressed(() => play(this.note));
+  }
+}
 
 function setup() {
   createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
   background(0);
   fill(255);
+  Tone.start();
+  for(note of scale) {
+    new Key(note);
+  }
 }
 
 function draw() {
 
+}
+
+
+function play(note) {
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease(note, "8n");
 }
