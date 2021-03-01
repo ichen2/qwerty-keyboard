0e2d61ca17250b54051d906af0167f42581ef5da
Ian <ichen2@lsu.edu>
2021-03-01 12:38:26 -0600

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+const SCREEN_WIDTH = 1280;
+const SCREEN_HEIGHT = 800;
+
+function setup() {
+  createCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+  background(0);
+  fill(255);
+}
+
+function draw() {
+
+}
